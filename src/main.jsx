a4651a6578e9898b0ef6a7e40998d6f9542a78e5
sameer-code <60299015+sameer-code@users.noreply.github.com>
2025-07-27 01:53:12 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { NavbarProvider } from './context/NavbarContext'; // ✅ import
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <NavbarProvider>
+      <App />
+    </NavbarProvider>
   </React.StrictMode>
-)
+);
