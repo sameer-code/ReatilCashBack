9e076c683373a25d3eb99a23ab4fd13c9d55eb14
sameer-code <60299015+sameer-code@users.noreply.github.com>
2025-07-27 00:24:33 +0530

    Update StoreList.jsx

--- src/components/StoreList.jsx
+++ src/components/StoreList.jsx
@@ -1,50 +1,31 @@
-import React from "react";
-
 const storeData = [
   {
     name: "Amazon",
     logo: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg",
+    link: "https://www.amazon.in/",
   },
   {
     name: "Flipkart",
     logo: "https://upload.wikimedia.org/wikipedia/commons/0/0b/Flipkart_logo.png",
+    link: "https://www.flipkart.com/",
   },
   {
     name: "Myntra",
     logo: "https://upload.wikimedia.org/wikipedia/commons/7/76/Myntra_Logo.png",
+    link: "https://www.myntra.com/",
   },
   {
     name: "Ajio",
     logo: "https://upload.wikimedia.org/wikipedia/commons/6/66/AJIO_Logo.png",
-  },
-  {
-    name: "Snapdeal",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/0/0c/Snapdeal_Logo.png",
-  },
-  {
-    name: "Tata Cliq",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/0/04/Tata_Cliq_Logo.png",
+    link: "https://www.ajio.com/",
   },
 ];
 
-const StoreList = () => {
-  return (
-    <div className="p-4 text-center">
-      <h2 className="text-xl font-bold mb-4">Popular Stores</h2>
-      <div className="flex flex-wrap justify-center gap-6">
-        {storeData.map((store, index) => (
-          <div key={index} className="flex flex-col items-center">
-            <img
-              src={store.logo}
-              alt={store.name}
-              className="w-24 h-16 object-contain mb-2"
-            />
-            <p className="text-sm">{store.name}</p>
-          </div>
-        ))}
-      </div>
+{storeData.map((store, index) => (
+  <a href={store.link} target="_blank" rel="noopener noreferrer" key={index}>
+    <div className="flex flex-col items-center hover:scale-105 transition">
+      <img src={store.logo} alt={store.name} className="w-24 h-16 object-contain mb-2" />
+      <p className="text-sm">{store.name}</p>
     </div>
-  );
-};
-
-export default StoreList;
+  </a>
+))}
